45b83957bc59b56e4450577ca6a6578cfa1fbda1
TranHoaiDuy <hoaiduy688@gmail.com>
2023-09-09 14:50:57 +0700

    chia mvc

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@
 // express package to be used in
 // our code
 const express = require("express");
+path = require('path');
 
 // Creates an express object
 const app = express();
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
 
 app.get("/", (req, res) => {
 
@@ -16,8 +22,18 @@ res.render("pages/index");
 });
 
 app.get("/login", (req, res) => {
+    // console.log(stringify(req))
     res.render("pages/login");
 })
+
+app.post("/login", (req, res) => {
+    const login = req.body.taikhoan;
+    const password = req.body.matkhau;
+
+    res.send('Tài khoản mật khẩu là: ' + login + ',' + password)
+    console.log(login)
+})
+
 
 // It configures the system to listen
 // to port 3000. Any number can be
